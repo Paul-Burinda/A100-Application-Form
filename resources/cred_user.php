7b8d1a6c568aa421e13b36e44b1e7a49e106a7b0
Mark <mfishera100@gmail.com>
2014-10-31 13:02:59 -0400

    stop tracking resources/cred_user.php

--- resources/cred_user.php
+++
@@ -1,6 +0,0 @@
-<?php
-            if (!defined('USER')) define('USER', 'root');
-            if (!defined('PASSWORD')) define('PASSWORD', '');
-            if (!defined('HOST')) define('HOST', 'localhost');
-            if (!defined('DATABASE_NAME')) define('DATABASE_NAME', 'applications_db');
-?>
